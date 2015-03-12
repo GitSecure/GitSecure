63b06dd2aacce431dd138d3488ea58d90975d676
Anthony Zotti <azotti1337@gmail.com>
2015-03-12 16:51:28 -0700

    Persist meta data to MongoDB

--- services/scraping.js
+++ services/scraping.js
@@ -2,7 +2,6 @@ var MongoClient = require('mongodb').MongoClient;
 var request = require('request');
 
 var options = {};
-//options.url = 'https://api.github.com?q=created%3A%3E2015-03-12&type=Repositories';
 options.url = 'https://api.github.com/search/repositories?q=sort=updated&order=desc&page=1&per_page=53';
 options.headers = {
   'User-Agent': 'request'
@@ -17,16 +16,13 @@ var formatUrl = function(fullName) {
   return {'url': 'https://github.com/' + fullName + '.git'};
 }
 
-MongoClient.connect('mongodb://127.0.0.1:27017/test2', function(err, db) {
+MongoClient.connect('mongodb://127.0.0.1:27017/test3', function(err, db) {
     var metaData = db.collection('metadata');
-    var urls = db.collection('urls');
 
     request(options, function(err, res, body) {
         var data = JSON.parse(body).items;
         for (var i = 0; i < data.length - 1;i++) {
-            console.log(data.length);
             metaData.insert(data[i], reportResults);
-            urls.insert(formatUrl(data[i].full_name), reportResults);
         }
     });
 });
