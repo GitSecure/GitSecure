c4410fd1a83c00c5d249dcd84627d5bfacb3f1e4
Joshua Newman <jonewman1020@gmail.com>
2015-03-12 12:08:02 -0700

    Added folder for gitHub data and laid out framework for parser

---
+++ js_helpers/readFilesForParsing.js
@@ -0,0 +1,15 @@
+//html parser library
+
+var APIKeys = {
+  twitter: {
+    consumerKey: 'XZY3ZAyRzem613wcfFsCWqnA3',
+    consumerSecret: '03CGds3Q0GCMSSLp3BjVo1zGEe0BS3zoabmh1NFTm3MCRlPvve',
+    owner: 'jjnewman10',
+    ownerID: 207420538,
+    accessLevel: 'Read-only'
+  }
+};
+
+var fileParser = function() {
+
+};
