4807677f6a711376b145030e27ad62ef4e7a3623
Marc Balaban <marc@marcbalaban.com>
2015-03-18 15:13:31 -0700

    app.js integrates all services. still need to work on parsing and deleting repo.

--- services/downloading/js_helpers/gitHubRepoGrabber.js
+++ services/downloading/js_helpers/gitHubRepoGrabber.js
@@ -4,18 +4,25 @@ var mainApp = require('../../../app.js');
 
 
 
-exports.readListOfFiles = function(urlList){
+exports.readListOfFiles = function(urlList, callback){
   var x = 0;
+  var directoryList = [];
   var strId = [];
 
-  for(var i = 0; i < urlList.length; i++){
-	  strId[i] = urlList[i].id.toString();
-	  mkdirp("git_data/" + strId[i], function(err){
-	  })
-	  clone(urlList[i].url, "git_data/" + strId[i]).then(function(repo){
-	  	mainApp.parseQueue.push(strId[x]);
-	  	mainApp.downloadQueue.shift();
-	  	x++;
-	  })
-	}
+  var onCompletion = function(){
+    for(var i = 0; i < urlList.length; i++){
+  	  strId[i] = urlList[i].id.toString();
+  	  mkdirp("git_data/" + strId[i], function(err){
+  	  })
+  	  clone(urlList[i].url, "git_data/" + strId[i]).then(function(repo){
+  	  	directoryList.push(strId[x]);
+  	  	x++;
+  	  })
+  	}
+    callback(directoryList);  
+  }
+
+  onCompletion();
+  
+
 }
--- services/scraping.js
+++ services/scraping.js
@@ -1,35 +1,37 @@
-var MongoClient = require('mongodb').MongoClient;
-var request = require('request');
-var mainApp = require('../app.js');
+exports.scrapeUrls = function(callback){
 
-var options = {};
-options.url = 'https://api.github.com/search/repositories?q=sort=updated&order=desc&page=1&per_page=53';
-options.headers = {
-  'User-Agent': 'request'
-};
+  var MongoClient = require('mongodb').MongoClient;
+  var request = require('request');
 
-var reportResults = function(err, result) {
-  if (err) console.log("ERROR: " + err);
-  console.log("RESULT: " + result);
+  var options = {};
+  options.url = 'https://api.github.com/search/repositories?q=sort=updated&order=desc&page=1&per_page=53';
+  options.headers = {
+    'User-Agent': 'request'
+  };
+
+  var reportResults = function(err, result) {
+    if (err) console.log("ERROR: " + err);
+    console.log("RESULT: " + result);
+  }
+
+  var formatUrl = function(fullName) {
+    return {'url': 'https://github.com/' + fullName + '.git'};
+  }
+
+
+  var db = MongoClient.connect('mongodb://127.0.0.1:27017/test4', 
+    function(err, db) {  
+      var metaData = db.collection('metadata');
+
+      request(options, function(err, res, body) {
+          var data = JSON.parse(body).items;
+          var dataGitUrls = [];
+          for (var i = 0; i < data.length - 1;i++) {
+            data[i].processed = false;
+            metaData.insert(data[i], reportResults);
+          }
+          // db.close();
+          callback()
+      });
+  });
 }
-
-var formatUrl = function(fullName) {
-  return {'url': 'https://github.com/' + fullName + '.git'};
-}
-
-
-exports.dataBase = MongoClient.connect('mongodb://127.0.0.1:27017/test4', 
-
-  function(err, db) {  
-    var metaData = db.collection('metadata');
-
-    request(options, function(err, res, body) {
-        var data = JSON.parse(body).items;
-        var dataGitUrls = [];
-        for (var i = 0; i < data.length - 1;i++) {
-          metaData.insert(data[i], reportResults);
-          dataGitUrls.push(data[i].id);
-
-        }
-    });
-});
