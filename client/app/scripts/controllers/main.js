16b1e7a6e0c9b875dd12e3ec4f9e248abdca64b7
Rebecca Vreeland <rebeccavreeland@gmail.com>
2015-03-19 16:10:38 -0700

    Nav buttons show active state.

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -11,4 +11,10 @@ angular.module('clientApp')
   .controller('MainCtrl', function ($scope) {
     $scope.scanned = '23,324';
     $scope.recovered = '306';
+  })
+
+  .controller('HeaderCtrl', function ($scope, $location) { 
+    $scope.isActive = function (viewLocation) { 
+      return viewLocation === $location.path();
+    };
   });
