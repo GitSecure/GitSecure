06a1c62e5111121b880875f11643a5ea740c3fb8
Rebecca Vreeland <rebeccavreeland@gmail.com>
2015-03-20 17:30:58 -0700

    Server serves static files. Client uses $interval to send get request to server every two seconds. Currently retrieving test data representing the number of repos scanned and hits found.

--- basic-server.js
+++ basic-server.js
@@ -8,6 +8,16 @@ var fs = require('fs');
 var app = express();
 var mainExecution = require("./app");
 
+//testing client -- fake count variable
+var counts = { scanned: 0, hits: 0 };
+
+var increaseCount = function() {
+  counts.scanned += 5;
+  counts.hits += 1;
+};
+
+setInterval(increaseCount, 1000);
+//end testing client
 
 var server = app.listen(3000, function(){
   var host = server.address().address;
@@ -15,13 +25,19 @@ var server = app.listen(3000, function(){
   console.log('example app listening at http://%s:%s', host, port);
 });
 
-// app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/client/dist'));
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('', function(req,res){
+app.get('/numbers', function(req,res){
+  //server should actually query the database for updated count info.
+  //need to add count property to database and update each time repo is 
+  //scanned or api key is found
+  res.send(201, counts);
 });
 
 app.post('/', function(req,res) {
 });
+
 
