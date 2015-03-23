f687404f13ee753f2a7c13abfddc7951c6a0f28e
Anthony Zotti <azotti1337@gmail.com>
2015-03-23 15:02:30 -0700

    Refactor downloading service
    Move gitHubRepoGrabber.js into the downloading service directory and remove /js_helper directory since there was only a single file in it.
    
    Remove unnecessary debugging code such as console.log's and comments
    
    Remove unnecessary require statements
    
    Refactor logging to include Github URL
    
    Remove duplicatative value formatting. Format all values at the start of
    gitHubRepoGrabber and save them to variables to use throughout function
    life cycle.

--- services/downloading/download.js
+++ services/downloading/download.js
@@ -1,12 +1,6 @@
-var gitHubRepoGrabber = require("./js_helpers/gitHubRepoGrabber");
-var readFilesForParsing = require("./js_helpers/readFilesForParsing");
-
 var metaData = GLOBAL.db.collection('metadata');
 metaData.find({processed: false}).toArray(function(err, body) {
   for (var i = 0;i < body.length;i++) {
-    console.log(body.git_url);
     gitHubRepoGrabber.readListofFiles(body[i].git_url, body[i].id);
-    console.log(body[i].git_url);
-    console.log(body[i].id);
   }
 });
