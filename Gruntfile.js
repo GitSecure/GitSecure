bf468975d9a6dfb99a9c62617c5b2064f63d6790
Camille Roussel <camille@rousselfamily.com>
2015-03-24 15:16:23 -0700

    initial rework of grunt in prep for azure deploy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,12 @@
-module.exports = function(grunt) {
+'use strict';
 
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-notify');
   grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -20,75 +19,32 @@ module.exports = function(grunt) {
       }
     },
 
-    clean: {
-      dist: 'dist/*',
-      result: 'results/*'
-    },
-
-    copy: {
-      client: {
-
-      },
-      server: {
-        src: ['basic-server.js'],
-        dest: 'dist/'
-      },
-      services: {
-        src: ['services/**'],
-        dest: 'dist/'
-      }
-    },
-
     jshint: {
-      files: [
-        'Gruntfile.js',
-        'basic-server.ks',
-        'services/parsing/*.js',
-        'Test/unit/parsing.js'
-      ],
+      files: ['**/*.js'],
+      options: {force: 'true'}
+    },
+
+    concurrent: {
+      tasks: ['nodemon', 'watch'],
       options: {
-        force: 'true',
+        logConcurrentOutput: true
       }
     },
 
-    express: {
+    nodemon: {
       dev: {
-        options: {
-          script: 'dist/basic-server.js'
-        }
+        script: 'server.js'
       }
     },
 
     watch: {
       gruntfile: {
-        files: 'Gruntfile.js',
-        tasks: 'jshint:gruntfile'
+        files: '**/*.js',
+        tasks: ['jshint', 'test']
       },
-      client: {
-      },
-      server: {
-        files: ['server/**'],
-        tasks: ['build', 'express:dev'],
-        options: {
-          spawn: false
-        }
-      },
-      services: {
-        files: ['services/**'],
-        tasks: [ 'build','express:dev'],
-      },
-      unitTests: {
-        files: ['Test/unit/parsing.js']
-      },
-      integrationTests: {
-
-      },
-      e2eTests: {
-
-      }
     },
   });
 
   grunt.registerTask('test', 'mochaTest');
-  grunt.registerTask('build', ['jshint', 'clean', 'copy']);
+  grunt.registerTask('default', ['concurrent']);
 };
