52520658fd4dab1df8a5634244c02e7af265264c
Jonathan Huang <grokut@gmail.com>
2015-03-25 09:56:52 -0700

    added angular client files

---
+++ auth-test/client/auth.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('GitSecure.AuthController', [])
+.controller('AuthController', function($scope, $state) {
+
+	$scope.email;
+	$scope.password;
+
+  $scope.signUp = function() {
+   
+  };
+
+  $scope.logIn = function() {
+   
+  };
+
+
+  
+});
