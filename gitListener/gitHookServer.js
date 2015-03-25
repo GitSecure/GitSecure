c01048d5a324c829dcf2bf9a13186b4cde30f63f
Camille Roussel <camille@rousselfamily.com>
2015-03-25 16:22:41 -0700

    initial auto-deploy setup.

--- gitListener/gitHookServer.js
+++ gitListener/gitHookServer.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var childProcess = require('child_process');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -14,4 +15,28 @@ app.use(bodyParser.json());
 
 app.post('/', function(req, res){
   console.log(req.body);
+  if (req.body.repository.full_name === 'graffiome/GitSecure') {
+    var execOptions = {
+      cwd: '../',
+    };
+    var gitCmd = 'git pull --rebase origin master';
+    var npmCmd = 'npm install';
+    var bowerCmd = 'bower install';
+    childProcess.exec(gitCmd, execOptions, function(err, stdout, stderr){
+      if (err) {console.error(err);}
+      if (stdout) {console.log(stdout);}
+      if (stderr) {console.warn(stderr);}
+      childProcess.exec(npmCmd, execOptions, function(err, stdout, stderr){
+        if (err) {console.error(err);}
+        if (stdout) {console.log(stdout);}
+        if (stderr) {console.warn(stderr);}
+      });
+      childProcess.exec(bowerCmd, execOptions, function(err, stdout, stderr){
+        if (err) {console.error(err);}
+        if (stdout) {console.log(stdout);}
+        if (stderr) {console.warn(stderr);}
+      });
+    });
+  }
+  res.end();
 });
