3833b95249b1b682831b2056cae153662674584f
Jonathan Huang <grokut@gmail.com>
2015-03-25 23:59:28 -0700

    (feat) github passport angular integration

---
+++ auth-test/client/controllers.js
@@ -0,0 +1,5 @@
+angular.module('main',[])
+.controller('mainController', function($scope){
+	$scope.email = 'happy'
+})
+.factory('mainly', function($rootScope){})
--- auth-test/test-server.js
+++ auth-test/test-server.js
@@ -2,8 +2,13 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+
 var cors = require('cors');
 var app = express();
+
+
+
+app.use(cors());
 var util = require('../server/utilities.js');
 // require('../server/app.js');
 
@@ -14,15 +19,13 @@ var GitHubStrategy = require('passport-github').Strategy;
 
 // var User = require('./db-user.js');
 
-var server = app.listen(3000, function(){
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('example app listening at http://%s:%s', host, port);
-});
+// app.use(express.cookieParser('secret'));
+// app.use(express.cookieSession());
+
 
 app.use(express.static(__dirname + '/client/'));
 
-app.get('/',function(req, res, next){
+app.get('/', function(req, res, next){
   console.log('hello')
   if (req.user || req.session.passport !== {}) {
     console.log('in the checkUser utility function, about to execute next()');
@@ -31,16 +34,26 @@ app.get('/',function(req, res, next){
     console.log('in the checkUser utility function, about to have the response object send "0"');
     res.send('0');
   }
-} )
+});
 
 // Check User Middleware
 
-// Prepare session for passport
-app.use(session({saveUninitialized: true, resave: true, secret: 'this is our secret'}));
+// User Serialization and Deserialization
+passport.serializeUser(function(user, done){
+  console.log('user about to be serialized', user);
+  done(null, user);
+});
 
-// Use passport to authenticate
-app.use(passport.initialize());
-app.use(passport.session());
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+// Prepare session for passport
+app.use(session({
+  saveUninitialized: true, 
+  resave: true, 
+  secret: 'this is our secret'
+}));
 
 // Use Github authentication
 passport.use(new GitHubStrategy({
@@ -49,14 +62,9 @@ passport.use(new GitHubStrategy({
   callbackURL: 'http://localhost:3000/auth/github/callback'
 }, function(accessToken, refreshToken, profile, done){
   console.log('accessToken', accessToken);
-  console.log('profile', profile);
-
-
-  // User.findOrCreate({githubId: profile.id, username: profile.displayName}, function(err, user) {
-  // 	console.log('done ', user);
-    return done();
-  // });
-
+   process.nextTick(function () {
+    return done(null, profile);
+  })
 }));
 
 // Passport session setup.
@@ -64,51 +72,34 @@ passport.use(new GitHubStrategy({
 // serialize users into and deserialize users out of the session. Typically,
 // this will be as simple as storing the user when serializing, and finding
 // the user when deserializing.
-passport.serializeUser(function(user, done){
-  console.log('user about to be serialized', user._id);
-  done(null, user);
-});
 
-passport.deserializeUser(function(id, done){
-  console.log('id to be used to deserialized', 'this is the id', id);
-  User.find({githubId: id._id}, function(err, user) {
-    if (err) { return err;}
-    done(err, user);
+// Use passport to authenticate
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/github',
+  passport.authenticate('github', {scope: 'write:repo_hook'} ));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', 
+    { sucessRedirect: '/', failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
   });
-});
-
 
 app.get('/auth/error', function(req, res){
   console.log('login failed in server');
   res.redirect('/');
 });
 
-app.get('/auth/github',
-  passport.authenticate('github'));
+app.get('/loggedin', function(req, res){
+  res.send(req.isAuthenticated() ? req.user : 'unauthorized');
+});
 
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/auth/error' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
-
-// app.get('/auth/github', function(req, res, next) {
-//   console.log('in the /auth path, trying to authenticate the user with passport');
-//   next();
-// },
-//   passport.authenticate('github', { failureRedirect: '/auth/error'}), function(req, res, next) {
-//   console.log('Authenticated through the github strategy, executing the next piece of middleware and redirecting to /main');
-//   console.log(req.user);
-//   res.redirect('/#/main');
-// });
-
-// // app.get('/auth/error', auth.error);
-// app.get('/auth/github/callback', 
-//   passport.authenticate('github', {failureRedirect: '/auth/error'})
-//   // auth.callback
-// );
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('example app listening at http://%s:%s', host, port);
+});
 
 
-app.use(bodyParser.json());
-app.use(cors());
